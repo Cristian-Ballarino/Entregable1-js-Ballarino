9988156e9a7bd6e08881dcce7268066236754712
cristian ballarino <cristianballarino1@gmail.com>
2025-02-13 02:27:58 -0300

    primer avance

---
+++ primer.js
@@ -0,0 +1,15 @@
+console.log("esto es un mensaje")
+// Solicitar al usuario que ingrese una contraseña
+let contraseña = prompt("Por favor, ingresa tu contraseña:");
+
+// Verificar la contraseña ingresada
+if (contraseña === "1234") {
+    alert("¡Acceso concedido!");
+} else {
+    alert("¡Contraseña incorrecta!.");
+}
+
+//primer boton
+document.getElementById('miboton').addEventListener('click', function() {
+    alert('¡primer boton!');
+});
